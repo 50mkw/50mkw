62404b54757517feb36567bd1c33ee701ff0e5ad
Patrick <490286710@qq.com>
2020-05-17 14:50:08 +0800

    ''

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 from app.home import home as home_blueprint
 
-app.register_blueprint(home_blueprint)
+app.register_blueprint(home_blueprint, url_prefix="/home")
--- app/home/views.py
+++ app/home/views.py
@@ -4,3 +4,7 @@ from . import home
 @home.route('/')
 def index():
     return 'Hello World!'
+
+@home.route('/test')
+def test():
+    return 'Hello World test!'
